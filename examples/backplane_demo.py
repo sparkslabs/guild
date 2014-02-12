8f48e442ac63f86eeea8cf2be6aba6b1cd409886
Michael Sparks <sparks,m@gmail.com>
2014-02-12 13:38:43 +0000

    How to use the Backplane - example/test case

---
+++ examples/backplane_demo.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import time
+
+from guild.actor import *
+from guild.components import Backplane, PublishTo, SubscribeTo, Printer
+
+class Producer(Actor):
+    @process_method
+    def process(self):
+        self.output("hello")
+    @late_bind_safe
+    def output(self, value):
+        pass
+
+
+Backplane("HELLO").start()
+p = Producer()
+pr = Printer()
+
+time.sleep(1)
+
+pub = PublishTo("HELLO")
+sub = SubscribeTo("HELLO")
+
+print "pub", pub, repr(pub), pub.input
+
+pipeline(p, pub)
+pipeline(sub, pr)
+
+start(p, pr, sub)
+
+time.sleep(1.0)
+
+stop(p, pr, sub)
+wait_for(p, pr, sub)
