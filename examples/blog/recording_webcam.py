fd64a2872f6ee533cdf76865246f4ad03b8005f9
Michael Sparks <sparks,m@gmail.com>
2014-03-09 21:42:23 +0000

    Addition of a simple webcam recorder/viewer
    Used on my blog here:
      http://www.sparkslabs.com/michael/blog/2014/03/07/guild---pipelinable-actors-with-late-binding

---
+++ examples/blog/recording_webcam.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#
+# See: http://www.sparkslabs.com/michael/blog/2014/03/07/guild---pipelinable-actors-with-late-binding/
+#
+
+
+import pygame, pygame.camera, time, os
+from guild.actor import *
+pygame.camera.init()
+
+class Camera(Actor):
+    def gen_process(self):
+        camera = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+        camera.start()
+        while True:
+            yield 1
+            frame = camera.get_image()
+            self.output(frame)
+            time.sleep(1.0/50)
+
+class Display(Actor):
+    def __init__(self, size):
+        super(Display, self).__init__()
+        self.size = size
+
+    def process_start(self):
+        self.display = pygame.display.set_mode(self.size)
+
+    @actor_method
+    def show(self, frame):
+        self.display.blit(frame, (0,0))
+        pygame.display.flip()
+
+    input = show
+
+class FrameStore(Actor):
+    def __init__(self, directory='Images', base='snap'):
+        super(FrameStore, self).__init__()
+        self.directory = directory
+        self.base = base
+        self.count = 0
+
+    def process_start(self):
+        try:
+            os.makedirs(self.directory)
+        except OSError, e:
+            if e.errno != 17:
+                raise
+
+    @actor_method
+    def input(self, frame):
+        self.count += 1
+        now = time.strftime("%Y%m%d-%H%M%S",time.localtime())
+        filename = "%s/%s-%s-%05d.jpg" % (self.directory, self.base, now, self.count)
+        pygame.image.save(frame, filename)
+        self.output(frame)
+
+camera = Camera().go()
+display = Display( (800,600) ).go()
+framestore = FrameStore().go()
+pipeline(camera, framestore, display)
+
+while 1:
+   time.sleep(30)
+
+stop(camera, framestore, display)
+wait_for(camera, framestore, display)
+
