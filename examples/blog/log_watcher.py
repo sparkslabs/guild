3f6e5b5f45c698f23b7e299d7ce9e29b6312e73b
Michael Sparks <sparks,m@gmail.com>
2014-03-10 13:14:24 +0000

    Example parallel log watch from blog post
    Modelled on the 99 bottle examples from the python concurrency wiki.

---
+++ examples/blog/log_watcher.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#
+# See: http://www.sparkslabs.com/michael/blog/2014/03/07/guild---pipelinable-actors-with-late-binding/
+#
+
+from guild.actor import *
+import re, sys, time
+
+class Follow(Actor):
+    def __init__(self, filename):
+        super(Follow, self).__init__()
+        self.filename = filename
+        self.f = None
+
+    def gen_process(self):
+        self.f = f = file(self.filename)
+        f.seek(0,2)   # seek to end
+        while True:
+            yield 1
+            line = f.readline()
+            if not line: # no data, so wait
+                time.sleep(0.1)
+            else:
+                self.output(line)
+
+    def onStop(self):
+        if self.f:
+            self.f.close()
+
+class Grep(Actor):
+    def __init__(self, pattern):
+        super(Grep, self).__init__()
+        self.regex = re.compile(pattern)
+
+    @actor_method
+    def input(self, line):
+        if self.regex.search(line):
+            self.output(line)
+
+class Printer(Actor):
+    @actor_method
+    def input(self, line):
+        sys.stdout.write(line)
+        sys.stdout.flush()
+
+follow1 = Follow("x.log").go()
+follow2 = Follow("y.log").go()
+grep = Grep("pants").go()
+printer = Printer().go()
+
+pipeline(follow1, grep, printer)
+pipeline(follow2, grep)
+wait_KeyboardInterrupt()
+stop(follow1, follow2, grep, printer)
+wait_for(follow1, follow2, grep, printer)
+
