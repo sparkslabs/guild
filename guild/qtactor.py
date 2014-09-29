abdf2ebfc47f07e204e5828954b21996ffd0ae57
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2014-09-29 08:45:53 +0100

    Don't use timer when running in own thread

--- guild/qtactor.py
+++ guild/qtactor.py
@@ -19,8 +19,9 @@ class QtActorMixin(ActorMixin):
             self._qtactor_rsock.fileno(), QtCore.QSocketNotifier.Read)
         self._qtactor_notifier.activated.connect(self._qtactor_do_queued)
         # signal to tell loop to exit
-        self._qtactor_running = threading.Event()
-        self._qtactor_running.set()
+        self._qtactor_kill = threading.Event()
+        self._qtactor_stopped = threading.Event()
+        self._qtactor_stopped.set()
         # if not a Widget, move to a Qt thread
         if isinstance(self, QtGui.QWidget):
             # widgets can't be moved to another thread
@@ -30,8 +31,6 @@ class QtActorMixin(ActorMixin):
             self._qtactor_thread = QtCore.QThread()
             self.moveToThread(self._qtactor_thread)
             self._qtactor_thread.started.connect(self._qtactor_run)
-        self._qtactor_proc_gen = None
-        self._qtactor_main_gen = None
 
     def _actor_notify(self):
         # run from any thread that needs to wake up our thread
@@ -51,38 +50,43 @@ class QtActorMixin(ActorMixin):
     def _qtactor_run(self):
         # get main process generator
         self._qtactor_main_gen = self._qtactor_main()
-        # use a timer to run generator in the background
-        self._qtactor_timer = QtCore.QTimer()
-        self._qtactor_timer.timeout.connect(self._qtactor_do_main)
-        self._qtactor_timer.start()
+        if self._qtactor_thread:
+            # run generator continuously
+            while True:
+                try:
+                    self._qtactor_main_gen.next()
+                except StopIteration:
+                    break
+                QtCore.QCoreApplication.processEvents()
+        else:
+            # use a timer to run generator in the background
+            self._qtactor_timer = QtCore.QTimer()
+            self._qtactor_timer.timeout.connect(self._qtactor_step)
+            self._qtactor_timer.start()
 
-    def _qtactor_do_main(self):
+    def _qtactor_step(self):
         try:
             self._qtactor_main_gen.next()
         except StopIteration:
             self._qtactor_timer.stop()
-            self._qtactor_running.clear()
 
     def _qtactor_main(self):
+        self._qtactor_stopped.clear()
         self.process_start()
         self.process()
         try:
-            self._qtactor_proc_gen = self.gen_process()
-        except:
-            self._qtactor_proc_gen = None
-        while self._qtactor_proc_gen:
-            if not self._qtactor_running.is_set():
-                self.onStop()
-                self._qtactor_running.set()
-                break
-            self._qtactor_proc_gen.next()
-            yield 1
+            for i in self.gen_process():
+                if self._qtactor_kill.is_set():
+                    self.onStop()
+                    break
+                yield 1
+        except AttributeError:
+            pass
+        self._qtactor_stopped.set()
 
     def stop(self):
-        if self._qtactor_running.is_set():
-            # process generator is still running
-            self._qtactor_running.clear()
-            self._qtactor_running.wait()
+        self._qtactor_kill.set()
+        self._qtactor_stopped.wait()
         if self._qtactor_thread:
             self._qtactor_thread.quit()
 
