d00b0a4c318d883c1a9d1f22598bae9092b28cda
Michael Sparks <sparks.m@gmail.com>
2022-07-17 15:42:30 +0100

    Possible interface and potential expected output from pre-processing

--- Sketches/05_queue_api/staged-preferred.cpp
+++ Sketches/05_queue_api/staged-preferred.cpp
@@ -2,20 +2,27 @@
 #include <queue>
 #include <string>
 
-// This is what we want to write
-// The result being something that looks like the code in staged.cpp
-// In python this occurs dynamically via a metaclass - which takes a class
-// and builds the actual class
+// This is what I want to write
+
+// The result being something that looks like the code in staged.cpp In
+// python this occurs dynamically via a metaclass - which takes a class and
+// builds the actual class
 
 // In C++ it's reasonable to assume that this would need to be a build step
 // - that takes the class definition and builds the appropriate code
 
-class Simple : Actor {
+class Actor {
 public:
+   void start();
+   void	join();
 
+};
+
+class Simple : public Actor {
+public:
     Simple() { }
 
-actormethods:
+//// actormethods:
     void hello(int x, int y) {
         std::cout << "Simple::hello" << " " << x << " " << y << "\n";
     }
@@ -23,10 +30,32 @@ actormethods:
         std::cout << "Simple::phrase" << " " << verb << " " << noun << "\n";
     }
 
+};
+
+
+class MoreComplex : Actor {
+public:
+
+    MoreComplex() { }
+
+//// actormethods:
+    void hello(int x, int y) {
+        std::cout << "Simple::hello" << " " << x << " " << y << "\n";
+    }
+    void phrase(std::string verb, std::string noun) {
+        std::cout << "Simple::phrase" << " " << verb << " " << noun << "\n";
+    }
+
+//// actorfunctions:
+    int withdrawal(int amount) {
+        return 5;
+    }
+
 // These are actually calls to actor methods, so we really could do this
-latebind:
+//// latebind:
     void output(std::string x);
 };
+
 
 
 int main(int argc, char **argv) {
@@ -37,7 +66,7 @@ int main(int argc, char **argv) {
     x.hello(5,10);
     x.phrase("hello", "world");
 
-    x.join()
+    x.join();
 
     return 0;
 }
--- Sketches/05_queue_api/staged.cpp
+++ Sketches/05_queue_api/staged.cpp
@@ -2,92 +2,124 @@
 #include <queue>
 #include <string>
 
-// Each exposed method gets a tag
-enum __Simple_tags {
-   HELLO,
-   PHRASE,
-};
+// Note: While this shows the pattern, lifetimes of values are likely to be an issue when this is threaded.
+// Needs confirming
 
-// Base class to allow  single inbound queue (which effectively serialises requests)
-struct arg_type { };
+namespace _Simple {
 
-// Matches the parameter signature of __Simple::hello
-struct __Simple_hello_arg_type : arg_type {
-   int x;
-   int y;
-};
-
-// Matches the parameter signature of __Simple::phrase
-struct __Simple_phrase_arg_type : arg_type {
-   std::string verb;
-   std::string noun;
-};
-
-// Could be created and named on a per class basis
-struct __Simple_MethodCall {
-    __Simple_tags tag;
-    arg_type* args;
-};
+    // Each exposed method gets a tag
+    enum function_tags {
+        HELLO,
+        PHRASE,
+    };
 
 
-class __Simple {
-public:
-    std::queue<__Simple_MethodCall> workqueue;  // Inbound calls come in here
+    // Base class to allow  single inbound queue (which effectively serialises requests)
+    struct arg_type {
+        arg_type() = default;
+    };
 
-    __Simple() { }
-    void hello(int x, int y) {
-        std::cout << "__Simple::hello" << " " << x << " " << y << "\n";
-    }
-    void phrase(std::string verb, std::string noun) {
-        std::cout << "__Simple::phrase" << " " << verb << " " << noun << "\n";
-    }
-
-    void dispatch() {
-        if (! workqueue.empty()) {
-           __Simple_MethodCall call;
-           call = workqueue.front();
-           workqueue.pop();
-
-           arg_type* raw_args = call.args;
-
-           if (call.tag == HELLO) {
-               std::cout << "TAG" << call.tag << "\n";
-               __Simple_hello_arg_type *args;
-               args =  (__Simple_hello_arg_type*) raw_args;
-               hello(args->x, args->y);
-           }
-           if (call.tag == PHRASE) {
-               std::cout << "TAG" << call.tag << "\n";
-               __Simple_phrase_arg_type *args;
-               args =  (__Simple_phrase_arg_type*) raw_args;
-               phrase(args->verb, args->noun);
-           }
+    // Matches the parameter signature of __Simple::hello
+    struct hello_args : arg_type {
+        int x;
+        int y;
+        hello_args(int a, int b) : x(a), y(b){}
+        ~hello_args() {
+            std::cout << "hello(" << x << ", " << y << "):" << long(this) <<" -- leaving scope\n";
         }
-    }
-};
+    };
+
+    // Matches the parameter signature of __Simple::phrase
+    struct phrase_args: arg_type {
+        std::string verb;
+        std::string noun;
+        phrase_args(std::string a, std::string b) : verb(a), noun(b){}
+        ~phrase_args() {
+            std::cout << "phrase_args(" << verb << ", " << noun << ":" << long(this) <<" -- leaving scope\n";
+        }
+    };
+
+    struct MethodCall {
+        function_tags tag;
+        arg_type* args;
+    };
+
+    // This is the "original" class. Note that it is essentially unchanged.
+    class Simple {
+    public:
+        Simple() { }
+        void hello(int x, int y) {
+            std::cout << "_Simple::Simple::hello" << " " << x << " " << y << "\n";
+        }
+        void phrase(std::string verb, std::string noun) {
+            std::cout << "_Simple::Simple::phrase" << " " << verb << " " << noun << "\n";
+        }
+
+    };
+
+    class Wrapper {
+    private:
+        Simple s;
+    public:
+
+        Wrapper() { }
+
+        std::queue<MethodCall> workqueue;  // Inbound calls come in here
+
+        void push(MethodCall m) {
+            workqueue.push(m);
+        }
+        
+        void dispatch() {
+            if (! workqueue.empty()) {
+                MethodCall call;
+                call = workqueue.front();
+                workqueue.pop();
+
+                arg_type* raw_args = call.args;
+
+                if (call.tag == HELLO) {
+                    std::cout << "TAG" << call.tag << "\n";
+                    hello_args *args;
+                    args =  (hello_args*) raw_args;
+                    s.hello(args->x, args->y);
+                }
+                if (call.tag == PHRASE) {
+                    std::cout << "TAG" << call.tag << "\n";
+                    phrase_args *args;
+                    args =  (phrase_args*) raw_args;
+                    s.phrase(args->verb, args->noun);
+                }
+            }
+        }
+    };
+}
 
 
 class Simple {
 private:
-    __Simple s;
+    _Simple::Wrapper s;
 public:
     Simple() { }
     void hello(int x, int y) {
         std::cout << "SimpleFRONT" << " " << x << " " << y << "\n";
-        __Simple_hello_arg_type args = { {}, x, y};
-        __Simple_MethodCall call = {HELLO, &args};
+        _Simple::hello_args args = { x, y };
+        _Simple::MethodCall call = {_Simple::HELLO, &args};
 
-        s.workqueue.push(call);
+        s.push(call);
         s.dispatch();
+
     }
     void phrase(std::string verb, std::string noun) {
         std::cout << "SimpleFRONT" << " " << verb << " " << noun << "\n";
-        __Simple_phrase_arg_type args = { {}, verb, noun };
-        __Simple_MethodCall call = { PHRASE, &args};
-        s.workqueue.push(call);
+        _Simple::phrase_args args = { verb, noun };
+        _Simple::MethodCall call = { _Simple::PHRASE, &args};
+        s.push(call);
         s.dispatch();
+        std::cout << __LINE__ << "\n";
     }
 };
+
 
 
 int main(int argc, char **argv) {
