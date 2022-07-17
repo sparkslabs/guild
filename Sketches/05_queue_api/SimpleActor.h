b858086842a95cfaaea87b23e118434576606bc7
Michael Sparks <sparks.m@gmail.com>
2022-07-17 15:42:52 +0100

    What a simpleactor would be nice to look like

---
+++ Sketches/05_queue_api/SimpleActor.h
@@ -0,0 +1,32 @@
+#ifndef _SAMPLE_CLASS_H_
+#define _SAMPLE_CLASS_H_
+
+#include <vector>
+#include <string>
+#include <stdio.h>
+
+
+class SimpleActor
+{
+public:
+
+    SampleClass();
+    string meth1();
+    int meth2(int v1);
+    void meth3(const string & v1, vector<string> & v2);
+    unsigned int meth4();
+
+private:
+    void * meth5(){return NULL};
+    string prop1;
+    int prop5;
+
+actormethods:
+    string acting();
+
+latebindmethods:
+    string acting();
+
+};
+
+#endif
