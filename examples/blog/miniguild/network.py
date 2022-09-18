83ddb584516e69693ef33fc2820127ee058267af
Michael Sparks <sparks.m@gmail.com>
2022-09-18 23:29:27 +0100

    Convert the Protocol to an Actor
    There's a number of practical benefits here, however it introduces a problem
    into the connection handler - in that it waits for data from the network, ignoring
    any messages from the protocol handler until it recieves data. This leads to
    some weirdness in the interaction that looks like this to a client:
    
        $ telnet 127.0.0.1 12345
        Trying 127.0.0.1...
        Connected to 127.0.0.1.
        Escape character is '^]'.
        Hello
        World
        Hello
    
    The solution is to have the connection handler poll the connection, allowing both
    sides to progress.

--- examples/blog/miniguild/network.py
+++ examples/blog/miniguild/network.py
@@ -10,18 +10,19 @@ import sys
 
 from miniguild import Scheduler, Actor
 
-class Protocol:
-    def __init__(self, output):
-        self.output = output
-
-    def input(self, data):
-        print('received "%s"' % data, file=sys.stderr)
-        if data:
-            print('sending data back to the client', file=sys.stderr)
-            self.output(data)
-        else:
-            print('no more data from', "self.client_address", file=sys.stderr)
-            self.output(None)
+class Protocol(Actor):
+    class Behaviour:
+        def input(self, data):
+            print("PH: OUTPUT FUNC", self.output)
+            print('PH: received "%s"' % data, file=sys.stderr)
+            if data:
+                print('PH: sending data back to the client', file=sys.stderr)
+                self.output(data)
+            else:
+                print('PH: no more data from', "self.client_address", file=sys.stderr)
+                self.output(None)
+                self._wrapper.stop()
+    actor_methods = ["input"]
 
 
 class ConnectionHandler(Actor):
@@ -32,27 +33,33 @@ class ConnectionHandler(Actor):
             self.running = True
 
         def main(self):
-            proto = Protocol(self.output)
+            proto = Protocol()
             try:
-                print('connection from', self.client_address, file=sys.stderr)
+                print('CH: connection from', self.client_address, file=sys.stderr)
                 # Receive the data in small chunks and retransmit it
                 while self.running:
                     yield 1
+                    print("CH: WAITING FOR DATA", self.connection, self.client_address)
                     data = self.connection.recv(16)
-                    proto.input(data)
+                    self.output(data)   # Send to protocol handler
+                    yield 1
                     
             finally:
                 # Clean up the connection
                 self.connection.close()
-            print('Connection Handler Shutdown', self.client_address, file=sys.stderr)
+            print('CH: Connection Handler Shutdown', self.client_address, file=sys.stderr)
 
-        def output(self, data):
+
+        def input(self, data):
+            # Input from the protocol handler
+            print("DATA FROM PROTOCOL", data)
             if data is None:
                 self.running = False
                 return
             self.connection.sendall(data)
 
-    
+    actor_methods = ["input"]
+
 
 class TCPServer:
     port = 12345
@@ -69,8 +76,13 @@ class TCPServer:
             print('waiting for a connection', file=sys.stderr)
             connection, client_address = sock.accept()
             ch = ConnectionHandler(connection, client_address)
-            ch.background()
+            ph = Protocol()
+            # Bidirectional links between protocol and client handlers:
+            ph.link("output", ch.input)
+            ch.link("output", ph.input)
 
+            ch.background()
+            ph.background()
 
 
 tcps = TCPServer()
